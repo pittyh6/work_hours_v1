b1680f882c4b7757db2e8502dc14870b59b71b15
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-10-08 14:09:13 +1100

    difine root / and start server

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ app.use(express.static('public'));
 app.use(express.json());
 
 //Define routes -> redirect to home page when open website
+app.get('/', function (req, res) {
+    res,render('index');
+})
+
+//start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
