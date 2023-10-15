838682b918d8c060c291c8ad1aef0c15211fbae5
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-10-16 10:39:44 +1100

    show and hide hamburg image

--- public/js/header.js
+++ public/js/header.js
@@ -3,23 +3,33 @@ const nav_items = document.querySelector('.nav-items')
 
 
 /*GET screen size and add the hide/show class*/
-const window_width = window.innerWidth
-if(window_width <= 1279){
-    nav_items.classList.add('hide')
-    nav_items.classList.remove('show')
-}else{
-    nav_items.classList.remove('hide')
-    nav_items.classList.add('show')
+window.onload = function () {
+    const window_width = window.innerWidth
+    if (window_width <= 1279) {
+        nav_items.classList.add('hide')
+        nav_items.classList.remove('show')
+        nav_btn.classList.remove('show')
+        nav_btn.classList.add('show')
+    } else {
+        nav_items.classList.remove('hide')
+        nav_items.classList.add('show')
+        nav_btn.classList.remove('show')
+        nav_btn.classList.add('hide')
+    }
 }
+
 /*--------------------------------------------*/
-nav_btn.addEventListener('click', ()=>{
-    if(nav_items.classList.contains('hide')){
+
+/*add class on hamburg menu click*/
+nav_btn.addEventListener('click', () => {
+    if (nav_items.classList.contains('hide')) {
         console.log("entou if")
         nav_items.classList.remove('hide')
         nav_items.classList.add('show')
-    }else{
+    } else {
         console.log("entou else")
         nav_items.classList.remove('show')
         nav_items.classList.add('hide')
     }
 })
+/*--------------------------------------------*/
