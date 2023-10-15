ffb06958937610d474afb97e30ced9a9fcbc8397
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-10-15 15:16:29 +1100

    get screen size

--- public/js/header.js
+++ public/js/header.js
@@ -2,6 +2,16 @@ const nav_btn = document.querySelector('.nav_btn')
 const nav_items = document.querySelector('.nav-items')
 
 
+/*GET screen size and add the hide/show class*/
+const window_width = window.innerWidth
+if(window_width <= 1279){
+    nav_items.classList.add('hide')
+    nav_items.classList.remove('show')
+}else{
+    nav_items.classList.remove('hide')
+    nav_items.classList.add('show')
+}
+/*--------------------------------------------*/
 nav_btn.addEventListener('click', ()=>{
     if(nav_items.classList.contains('hide')){
         console.log("entou if")
