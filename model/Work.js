6b741663f5a1b06b1acf3a3a93e22f0361f7a2e7
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-11-06 08:39:43 +1100

    finalize schema and create model

--- model/Work.js
+++ model/Work.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
+//create schema for workdb
 const workSchema = new Schema({
     name_user: String,
-    work_day: Date,
-    punch_in: String,
-    punch_out: String,
-    break_in: String,
-    break_out: String,
+    work: [
+        day: Date,
+        punch_in: String,
+        punch_out: String,
+        break_in: String,
+        break_out: String,
+    ]
 })
+
+//create model
+const Work = model('Work',workSchema);
+export default Work;
