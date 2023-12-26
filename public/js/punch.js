12cd12324875e6fa39b69df746428467448e0065
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-12-26 14:38:03 +1100

    create new punch and save.. Still not working yet

--- public/js/punch.js
+++ public/js/punch.js
@@ -1,17 +1,12 @@
+
+
 const btn_clock_in = document.querySelector('#clock-in')
 const btn_clock_out = document.querySelector('#clock-out')
 const btn_break_start = document.querySelector('#break-start')
 const btn_break_end = document.querySelector('#break-end')
-const name_user = document.querySelector('#name-user')
-const id_user = document.querySelector('#id-user')
-
-/*function punchTime(event) {
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes();
-
-    console.log("Punched event:" + event + " Date: " + date + " Time: " + time)
-}*/
+const name_user_value = document.querySelector('#name_user').innerHTML
+const id_user = document.querySelector('#id_user')
+const id_user_value = document.querySelector('#id_user').innerHTML
 
 const currentDay = new Date()
 var date = currentDay.getFullYear() + '-' + (currentDay.getMonth() + 1) + '-' + currentDay.getDate();
@@ -19,7 +14,7 @@ var time = currentDay.getHours() + ":" + currentDay.getMinutes();
 var dayOfWeek = ["Sunday","Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"]
 var weekDay = dayOfWeek[currentDay.getDay()]
 
-console.log(weekDay)
+
 btn_clock_in.addEventListener('click', function (e) {
    // punchTime(e.target.id)
     //const currentDay = new Date()
@@ -27,20 +22,29 @@ btn_clock_in.addEventListener('click', function (e) {
     console.log("date: " + date)
     console.log("time: " + time)
     console.log("weekDay: " + weekDay)
-
-    /*try {
+    console.log("name_user_value: " + name_user_value)
+   
+    try {
         const newPunchIn = new Work({
-            _id: 100001,
-            name_user: 'Priscila',
-            day: 26 - 12 - 2023,
-            week_day: "Tuesday",
-            punch_in: '09:50',
+            _id: id_user_value,
+            name_user: name_user_value,
+            day: date,
+            week_day: weekDay,
+            punch_in: time,
             punch_out: '18:50',
             break_in: '12:00',
             break_out: '12:30',
         })
-    }*/
+        newPunchIn.save().then(savePunchIn => {
+            console.log('Punch in saved:', savedPunchIn);
+        }).catch(saveError => {
+            console.error('Error saving punch in:', saveError);
+        });
+    }catch(error){
+        console.log("Error saving new punchIn: ", error)
+    }
 })
+
 btn_clock_out.addEventListener('click', function (e) {
     punchTime(e.target.id)
 })
